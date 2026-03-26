19b97d8a91f34809273c0bc7709f54deb934cf9b
Pirasak S. <guzzguzzguzz96@gmail.com>
2026-03-26 15:17:22 +0700

    commit meta pixel
    - add meta pixel to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -56,6 +56,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
   const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || "GTM-WXWZ2MFN";
+  const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
   const jsonLdEducationalOrg = {
     "@context": "https://schema.org",
@@ -156,6 +157,23 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} scroll-smooth`}
     >
       <head>
+        {META_PIXEL_ID && (
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '${META_PIXEL_ID}');
+      fbq('track', 'PageView');
+    `}
+          </Script>
+        )}
+
         {GTM_ID && (
           <Script id="gtm-script" strategy="afterInteractive">
             {`
